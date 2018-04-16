ba399665243d00085776df3d157a963d273b83fd
ChangJoo Park <pcjpcj2@gmail.com>
2018-04-17 00:01:16 +0900

    Add login scenarios

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,9 +10,38 @@ class LoginScreen extends StatefulWidget {
 }
 
 class LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormState> formKey = new GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  String _email, _password = '';
+  bool _isLoading = false;
+
+  void _submit() {
+    final form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      print("form show");
+      print(_email);
+      print(_password);
+
+      setState(() => _isLoading = true);
+
+      new Timer(const Duration(seconds: 3), () {
+        if (_email == 'a' && _password == 'b') {
+          Navigator.of(context).pushReplacementNamed('/home');
+          return;
+        }
+        scaffoldKey.currentState
+            .showSnackBar(new SnackBar(content: new Text('로그인 실패, 이메일은 a, 비밀번호는 b')));
+        setState(() => _isLoading = false);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      key: scaffoldKey,
       appBar: new AppBar(
         title: new Text('Login'),
       ),
@@ -18,10 +49,12 @@ class LoginScreenState extends State<LoginScreen> {
         top: false,
         bottom: false,
         child: new Form(
+          key: formKey,
           child: new Column(
             children: <Widget>[
               new TextFormField(
                 autofocus: true,
+                onSaved: (val) => _email = val,
                 decoration: new InputDecoration(
                   border: const UnderlineInputBorder(),
                   icon: const Icon(Icons.email),
@@ -31,6 +64,7 @@ class LoginScreenState extends State<LoginScreen> {
               ),
               const SizedBox(height: 24.0),
               new TextFormField(
+                onSaved: (val) => _password = val,
                 obscureText: true,
                 decoration: new InputDecoration(
                   border: const UnderlineInputBorder(),
@@ -39,12 +73,10 @@ class LoginScreenState extends State<LoginScreen> {
                 ),
               ),
               const SizedBox(height: 24.0),
-              new RaisedButton(
-                onPressed: () {
-                  Navigator.of(context).pushReplacementNamed('/home');
-                },
-                child: new Text('Login'),
-              ),
+              _isLoading
+                  ? new CircularProgressIndicator()
+                  : new RaisedButton(
+                      onPressed: _submit, child: new Text('Login'))
             ],
           ),
         ),
