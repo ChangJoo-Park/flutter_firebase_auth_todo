ce7ea8e801a053aa56b4cf9e012578530371e93a
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-04-18 21:53:07 +0900

    Temporaily add ensureLoggedin

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,79 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = new GoogleSignIn();
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new HomeScreenState();
+  }
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  GoogleSignInAccount _user;
+
+  Future<Null> _ensureLoggedIn() async {
+    GoogleSignInAccount user = _googleSignIn.currentUser;
+
+    if (user == null) {
+      user = await _googleSignIn.signInSilently();
+    }
+
+    if (user == null) {
+      await _googleSignIn.signIn();
+      // analytics.logLogin();
+    }
+
+    if (await _auth.currentUser() == null) {
+      GoogleSignInAuthentication credentials =
+          await _googleSignIn.currentUser.authentication;
+      await _auth.signInWithGoogle(
+        idToken: credentials.idToken,
+        accessToken: credentials.accessToken,
+      );
+    }
+    if (user != null) {
+      setState(() {
+        this._user = user;
+      });
+      print('user has exists');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    (() async {
+      print("before ensure logged in");
+      await _ensureLoggedIn();
+      print("after ensure logged in");
+    })();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Home"),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.exit_to_app),
+            onPressed: () async {
+              await _googleSignIn.signOut();
+              await _auth.signOut();
+              Navigator.of(context).pushReplacementNamed('/login');
+            },
+          ),
+        ],
       ),
       body: new Center(
-        child: new Text("Welcome home!"),
+        child: new Text(''),
       ),
     );
   }
