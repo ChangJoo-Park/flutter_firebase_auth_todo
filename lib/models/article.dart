7b5fad0c896a25c347b589042d59913d7fac3ded
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2018-04-19 00:00:57 +0900

    Add article model

---
+++ lib/models/article.dart
@@ -0,0 +1,11 @@
+class Article {
+  String id;
+  String title;
+  String body;
+
+  Article(String id, String title,String body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+  }
+}
